5e60e842c5c047c6d1cb1981d4ae47435a64eb5f
Ryokx <59834196+Ryokx@users.noreply.github.com>
2021-09-26 18:31:41 +0200

    src

---
+++ src/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  owners: [],
+  nodes: [{
+    identifier: 'NODE-MAIN-2',
+    host: 'lavalink-repl.ahmasa.repl.co',
+    port: 443,
+    secure: true,
+  }],
+  prefix: '!',
+  updateSlash: false,
+};
